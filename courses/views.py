fadc7344be06b2d41f626d288dd0de878725d5c7
Daniel <twistytiger124@gmail.com>
2024-10-01 16:09:07 +0100

    updated the database

--- courses/views.py
+++ courses/views.py
@@ -16,7 +16,7 @@ def course_registration_view(request: HttpRequest) -> HttpResponse:
         form = CourseRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('thank_you')  # Redirect after successful submission
+            return redirect('thank_you.html')  # Redirect after successful submission
     else:
         form = CourseRegistrationForm()
     return render(request, 'course_registration.html', {'form': form})
--- myproject/settings.py
+++ myproject/settings.py
@@ -54,10 +54,18 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_database_name',
+        'USER': 'postgres',
+        'PASSWORD': '20200294095',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
+
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
